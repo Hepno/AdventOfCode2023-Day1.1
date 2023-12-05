d53f3c147f7b5819334f9e375b7a75e55a734b08
hepno <me@hepno.dev>
2023-12-05 10:20:15 +0100

    feat: basic example

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        String[] examples = new String[]{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"};
+        int value = 0;
+
+        for (String s : examples) {
+            String replacedString = s.replaceAll("[^\\d.]", "");
+            String firstDigit = Character.toString(replacedString.charAt(0));
+            String secondDigit = replacedString.substring(replacedString.length() - 1);
+            int number = Integer.parseInt(firstDigit + secondDigit);
+            value += number;
+        }
+        System.out.println(value);
 
     }
 

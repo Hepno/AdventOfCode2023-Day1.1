870c871079db7e7edcee5d06004d7b4bb53c80b6
hepno <me@hepno.dev>
2023-12-05 10:40:52 +0100

    feat: read input.txt

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        String[] examples = new String[]{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"};
         int value = 0;
 
-        for (String s : examples) {
+        // Read input.txt
+        Main main = new Main();
+        List<String> strings = main.readFile("input.txt");
+
+        // Calculate answer
+        for (String s : strings) {
             String replacedString = s.replaceAll("[^\\d.]", "");
             String firstDigit = Character.toString(replacedString.charAt(0));
             String secondDigit = replacedString.substring(replacedString.length() - 1);
             int number = Integer.parseInt(firstDigit + secondDigit);
             value += number;
         }
+
         System.out.println(value);
 
+    }
+
+    public List<String> readFile(String fileName) {
+        File file = new File(getClass().getResource(fileName).getPath());
+        List<String> lines = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                lines.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
     }
 
 }
